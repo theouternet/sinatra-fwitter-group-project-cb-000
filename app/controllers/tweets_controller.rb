dfe495ad2b266689b9999bb2f1156a05b5a2dc12
theouternet <jrougeau@gmail.com>
2020-04-13 13:04:34 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,14 +2,60 @@ class TweetsController < ApplicationController
 
 
 get '/tweets' do
-  
+  if logged_in?
   @tweets = Tweet.all
   erb :'/tweets/tweets'
-  
+  else
+    redirect to '/login'
+  end 
+end 
+
+get '/tweets/new' do 
+if logged_in?
+  erb :'/tweets/new'
+else 
+  redirect to '/login'
+end 
+end
+
+post '/tweets' do 
+if logged_in?
+  if params[:content] == ""
+    redirect to "/tweets/new"
+  else 
+    @tweet = current_user.tweets.build(content: params[:content])
+if @tweet.save 
+  redirect to "/tweets/#{tweet.id}"
+else 
+  redirect to "/tweets/new"
+end
+end 
+else
+  redirect to '/login'
+end 
+end 
+
+get 'tweets/:id' do 
+  if logged_in?
+  @tweet = Tweet.find_by_id(params[:id])
+  erb :'tweets/show_tweet'
+  else 
+    redirect to '/login'
+  end 
+end 
+
+get '/tweets/:id/edit' do
+if logged_in? 
+  @tweet = Tweet.find_by_id(params[:id])
+  if @tweet && @tweet.user == current_user
+    erb :'tweets/edit_tweet'
+else 
+  redirect to '/tweets'
+end 
+else 
+  redirect to '/login'
+end 
 end 
 
 
-
-
-
-end
+end 
